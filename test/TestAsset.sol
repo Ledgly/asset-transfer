0be3159a9fa4fc5fa59512f9c5c4828996ec85aa
vincenthang <gghvince@gmail.com>
2018-04-01 17:12:14 -0400

    var change

--- test/TestAsset.sol
+++ test/TestAsset.sol
@@ -7,9 +7,13 @@ import "../contracts/Asset.sol";
 contract TestAsset{
 	Asset asset = Asset(DeployedAddresses.Asset());
 
+	address a1 = 0x96576cc0E57d6303DFe763Cb7f060EFCFFaaa2f8;
+
+	address a2 = 0xbdE17789B3FC44A85EF73b3be5158610121BEBeB;
+
 	function testOwnership() public {
 		address owner = asset.getOwner();
-		Assert.equal(owner, 0x96576cc0E57d6303DFe763Cb7f060EFCFFaaa2f8, "Owner should be the first account.");
+		Assert.equal(owner, a1, "Owner should be the first account.");
 	}
 
 
